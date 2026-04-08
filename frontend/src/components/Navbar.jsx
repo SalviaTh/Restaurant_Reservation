db7941d819c29d7ef34be6519ef2cf2a64175103
SalviaTh <salviathingbaijam@gmail.com>
2026-04-09 00:10:30 +0530

    changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
               </Link>
             ))}
           </div>
-          <button className="menuBtn">Change mode</button>
+          {/* <button className="menuBtn">Change mode</button> */}
           <button className="menuBtn">OUR MENU</button>
         </div>
         <div className="hamburger" onClick={()=> setShow(!show)}>
