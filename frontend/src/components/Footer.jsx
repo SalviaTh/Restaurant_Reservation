dd626a991ada236d713c83e88181cc8b6af340b3
SalviaTh <salviathingbaijam@gmail.com>
2026-04-08 20:44:34 +0530

    Initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">SALVIA</div>
+          <div className="right">
+            <p>Imphal East,Manipur</p>
+            <p>Open: 05:00 AM - 10:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Thingbaijam Salvia</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Thingbaijam Salvia.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
